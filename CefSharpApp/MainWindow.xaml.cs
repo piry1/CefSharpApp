9ede73c85731511364a870aaa72157efef662d59
Piotr <piry3943@gmail.com>
2017-05-03 17:55:37 +0200

    Add project files.

---
+++ CefSharpApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+
+
+namespace CefSharpApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChromiumWebBrowser ChromeBrowser;
+        string StartPage = string.Format(@"{0}\html-resources\html\index.html", AppDomain.CurrentDomain.BaseDirectory);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+
+        private void InitializeChromium()
+        {
+            // Create browser
+            Cef.Initialize(new CefSettings());
+            ChromeBrowser = new ChromiumWebBrowser();
+            ChromeBrowser.IsBrowserInitializedChanged += new DependencyPropertyChangedEventHandler(LoadStartPage);
+
+            // registrate js objects
+            ChromeBrowser.RegisterJsObject("cefCustomObject", new CefObj());
+
+            // Add to window
+            con.Content = ChromeBrowser;
+
+            // Set browser settings
+            BrowserSettings brSettings = new BrowserSettings();
+            brSettings.FileAccessFromFileUrls = CefState.Enabled;
+            brSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            ChromeBrowser.BrowserSettings = brSettings;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+
+        private void LoadStartPage(object sender, DependencyPropertyChangedEventArgs e)
+        {   
+            if (ChromeBrowser.IsInitialized)
+                ChromeBrowser.Load(StartPage);
+        }
+
+    }
+}
---
+++ CefSharpApp/cefObj.cs
@@ -0,0 +1,34 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System.Diagnostics;
+using System.Windows;
+
+namespace CefSharpApp
+{
+    public class CefObj
+    {
+        // Declare a local instance of chromium and the main form in order to execute things from here in the main thread
+        private static ChromiumWebBrowser _instanceBrowser = null;
+        // The form class needs to be changed according to yours
+        private static MainWindow _instanceMainForm = null;
+
+
+        public CefObj()
+        {
+          //  _instanceBrowser = originalBrowser;
+           // _instanceMainForm = mainForm;
+        }
+
+        public void showDevTools()
+        {
+            //  _instanceBrowser.ShowDevTools();
+            MessageBox.Show("ddsad");
+        }
+
+        public void opencmd()
+        {
+            ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
+            Process.Start(start);
+        }
+    }
+}
